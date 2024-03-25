60cb11c5df19551df151917995268c743cf2cbc2
Krzysztof Pawlowski <krzysztofpeiv@o2.pl>
2024-03-25 13:15:11 +0100

    added player button logic

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,24 @@
-export default function Player({name, symbol}){
-    return <>
-        <li>
-            <span className="player">
-              <span class="player-name">{name}</span>
-              <span class="player-symbol">{symbol}</span>
-            </span>
-            <button>Edit</button>
-        </li>
-    </>
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+  let playerName = <span class="player-name">{name}</span>
+  let btnCaption = "Edit"
+
+  if(isEditing){
+    playerName =  <input type="text" value={name} required />
+    btnCaption="Save"
+  }
+  return (
+    <li>
+      <span className="player">
+        {playerName}
+        <span class="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{btnCaption}</button>
+    </li>
+  );
 }
