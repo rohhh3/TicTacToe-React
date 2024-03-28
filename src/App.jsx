d5ecc0d8756865b045ec8c1f088b4599ab081f2e
Krzysztof Pawlowski <krzysztofpeiv@o2.pl>
2024-03-28 15:40:12 +0100

    reduced states

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,23 @@ import Player from "./components/Player"
 import Gameboard from "./components/Gameboard"
 import Log from "./components/Log"
 import { useState } from "react"
+
+function deriveActivePlayer(gameTurns){
+  let currentPlayer = 'X'
+  if(gameTurns.length > 0 && gameTurns[0].player === 'X'){
+    currentPlayer = 'O'
+  }
+  return currentPlayer
+}
+
 function App() {
-  const [activePlayer, setActivePlayer] = useState('X')
   const [gameTurns, setGameTurns] = useState([])
 
+  const activePlayer = deriveActivePlayer(gameTurns)
+
   function handleSelectSquare(rowIndex, colIndex){
-    setActivePlayer((curActivePlayer) =>  curActivePlayer === 'X' ? 'O' : 'X');
     setGameTurns(prevTurns => {
-      let currentPlayer = 'X'
-      if(prevTurns.length > 0 && prevTurns[0].player === 'X'){
-        currentPlayer = 'O'
-      }
+      const currentPlayer = deriveActivePlayer(prevTurns)
       const updatedTurns = [{ square: { row: rowIndex, col: colIndex}, player:  currentPlayer}, ...prevTurns]
       return updatedTurns
     });
